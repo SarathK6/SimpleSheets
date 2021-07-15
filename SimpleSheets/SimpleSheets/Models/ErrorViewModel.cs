7a6b2f01f166dd8c7da12913e660fe5983432473
Nithin-Bharadwaj <nithin.bharadwaj@inovarconsulting.co.in>
2021-07-15 22:34:47 +0530

    Added TimeSheets Project

---
+++ SimpleSheets/SimpleSheets/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleSheets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
