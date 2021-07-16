15144cf6b0bd62becaedd771784b1bb2f76ca712
Nithin-Bharadwaj <nithin.bharadwaj@inovarconsulting.co.in>
2021-07-16 14:27:55 +0530

    Added DB Project and Dapper ORM

---
+++ SimpleSheets/TimesheetsDB/dbo/Tables/Projects.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Projects] (
+    [id]    INT            IDENTITY (1, 1) NOT NULL,
+    [title] NVARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ SimpleSheets/TimesheetsDB/dbo/Tables/TimeType.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[TimeType] (
+    [id]       INT            IDENTITY (1, 1) NOT NULL,
+    [timeType] NVARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
