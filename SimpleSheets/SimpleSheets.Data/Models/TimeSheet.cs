b01ec4f1f1de1994dabcc95144035cc81c8e8750
sarathk6 <sairamsarath800@gmail.com>
2021-07-20 16:36:13 +0530

    moved valiadtion class file to new folder under Models

--- SimpleSheets/SimpleSheets.Data/Models/TimeSheet.cs
+++ SimpleSheets/SimpleSheets.Data/Models/TimeSheet.cs
@@ -39,12 +39,5 @@ namespace SimpleSheets.Data.Models
         public DateTime TimeSheetEntryDate { get; set; }
     }
 
-    public class CustomAdmissionDate : ValidationAttribute
-    {
-        public override bool IsValid(object value)
-        {
-            DateTime dateTime = Convert.ToDateTime(value);
-            return dateTime <= DateTime.Now;
-        }
-    }
+   
 }
